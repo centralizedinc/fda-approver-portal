14f4158df93055e79d38a3f0f95aa8334053ef03
Mark <markjhonpaul.quijom@gmail.com>
2019-02-26 15:14:34 +0800

    update

--- src/router.js
+++ src/router.js
@@ -8,6 +8,30 @@ import store from './store'
 
 Vue.use(Router)
 
+function forPrinting(to, from, next) {
+  store.dispatch('IS_FOR_PRINTING').then((isForPrinting) => {
+    if (isForPrinting) {
+      next()
+    } else {
+      next('/app')
+    }
+  }).catch(err => {
+    next('/app')
+  })
+}
+
+function notForPrinting(to, from, next) {
+  store.dispatch('IS_FOR_PRINTING').then((isForPrinting) => {
+    if (!isForPrinting) {
+      next()
+    } else {
+      next('/app')
+    }
+  }).catch(err => {
+    next('/app')
+  })
+}
+
 export default new Router({
   routes: [{
       path: '/',
@@ -16,7 +40,8 @@ export default new Router({
       children: [{
         path: '',
         name: 'Login',
-        component: () => import('@/views/Login.vue')
+        component: () =>
+          import ('@/views/Login.vue')
       }],
       beforeEnter: (to, from, next) => {
         new AuthAPI().checkAuth(store.state.user_session.token, (isAuth) => {
@@ -42,41 +67,56 @@ export default new Router({
         })
       },
       children: [{
-        path: '',
-        name: 'Dashboard',
-        component: () => import('@/views/app/UserPortfolio.vue')
-      }, 
-      {
-        path: 'print',
-        name: 'Print',
-        component: () => import('@/views/app/Print/Print.vue')
-      },
-      {
-        path: 'print/history',
-        name: 'Print History',
-        component: () => import('@/views/app/Print/History.vue')
-      },
-      {
-        path: 'inbox',
-        name: 'Inbox',
-        component: () => import('@/views/app/Inbox/Inbox.vue')
-      }, {
-        path: 'participated',
-        name: 'Participated',
-        component: () => import('@/views/app/Participated/Participated.vue')
-      }, {
-        path: 'unassigned',
-        name: 'Unassigned',
-        component: () => import('@/views/app/Unassigned/Unassigned.vue')
-      }, {
-        path: 'evaluation',
-        name: 'Evaluation',
-        component: () => import('@/views/app/Evaluation/EvaluationForm.vue')
-      },{
-        path: 'profile',
-        name: 'Profile Management',
-        component: () => import('@/views/app/profile.vue')
-      }]
+          path: '',
+          name: 'Dashboard',
+          component: () =>
+            import ('@/views/app/UserPortfolio.vue')
+        },
+        {
+          path: 'print',
+          name: 'Print',
+          beforeEnter: forPrinting,
+          component: () =>
+            import ('@/views/app/Print/Print.vue')
+        },
+        {
+          path: 'print/history',
+          name: 'Print History',
+          beforeEnter: forPrinting,
+          component: () =>
+            import ('@/views/app/Print/History.vue')
+        },
+        {
+          path: 'inbox',
+          name: 'Inbox',
+          beforeEnter: notForPrinting,
+          component: () =>
+            import ('@/views/app/Inbox/Inbox.vue')
+        }, {
+          path: 'participated',
+          name: 'Participated',
+          beforeEnter: notForPrinting,
+          component: () =>
+            import ('@/views/app/Participated/Participated.vue')
+        }, {
+          path: 'unassigned',
+          name: 'Unassigned',
+          beforeEnter: notForPrinting,
+          component: () =>
+            import ('@/views/app/Unassigned/Unassigned.vue')
+        }, {
+          path: 'evaluation',
+          name: 'Evaluation',
+          beforeEnter: notForPrinting,
+          component: () =>
+            import ('@/views/app/Evaluation/EvaluationForm.vue')
+        }, {
+          path: 'profile',
+          name: 'Profile Management',
+          component: () =>
+            import ('@/views/app/profile.vue')
+        }
+      ]
     },
     {
       path: '/about',
@@ -84,7 +124,8 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import ( /* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })

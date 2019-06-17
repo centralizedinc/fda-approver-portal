90ddf65889a36684fac8d4aa99dc3f9f6ff80098
Mark <markjhonpaul.quijom@gmail.com>
2019-06-17 09:20:09 +0800

    update payment for certificate

--- src/api/PaymentApi.js
+++ src/api/PaymentApi.js
@@ -58,7 +58,7 @@ export default class PaymentAPI {
     }
 
     getCertificateFees(details) {
-        return axios.get(`payments/fees/certificate/${details.case_no}/${details.product_type}`)
+        return axios.post("payments/fees/certificate", details)
     }
 
     savePayment(fullDetails, cb) {
